ddbf4cc2663324b028f445f049df1058794ffff2
hanjiayi02 <hanjiayi02@meituan.com>
2021-02-03 16:11:28 +0800

    init

---
+++ stylelint.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  'defaultSeverity': 'error',
+  'extends': [
+    'stylelint-config-standard',
+  ],
+  'rules': {
+    'rule-empty-line-before': null,
+    'function-comma-space-after': 'always',
+    'selector-list-comma-newline-after': 'always',
+    'color-hex-case': 'upper',
+    'color-hex-length': 'long',
+    'declaration-empty-line-before': 'never',
+    'comment-empty-line-before': 'never',
+    'block-closing-brace-empty-line-before': 'never',
+    'no-eol-whitespace': true,
+    'declaration-bang-space-before': 'always',
+    'comment-whitespace-inside': 'always',
+    'declaration-block-no-duplicate-properties': true,
+    'value-list-comma-space-after': 'always',
+    'font-family-no-missing-generic-family-keyword': null,
+    'no-descending-specificity': null,
+    'value-list-comma-newline-after': null,
+  }
+};
